32bdc7e8cbc7cc5de5b8c24cf1c09fed36fae07a
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-24 13:50:08 -0400

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,22 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <nav>
+    <nav className={styles.container}>
       {user ?
-        <ul>
+        <ul className={styles.navUl}>
           <li>Welcome, {user.name}</li>
-          <li><Link to="/profiles">Profiles</Link></li>
-          <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          <li><Link to="/changePassword">Change Password</Link></li>
-          <li><Link to="/addVet">Add Vet Visit</Link></li>
-          <li><Link to="/addPet">Add Pet</Link></li>
+          <li><Link to="/profiles" className={styles.navLink}>Profiles</Link></li>
+          <li><Link to="" onClick={handleLogout} className={styles.navLink}>LOG OUT</Link></li>
+          <li><Link to="/changePassword" className={styles.navLink}>Change Password</Link></li>
+          <li><Link to="/addVet" className={styles.navLink}>Add Vet Visit</Link></li>
+          <li><Link to="/addPet" className={styles.navLink}>Add Pet</Link></li>
         </ul>
       :
-        <ul>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
+        <ul className={styles.navUl}>
+          <li><Link to="/login" className={styles.navLink}>Log In</Link></li>
+          <li><Link to="/signup" className={styles.navLink}>Sign Up</Link></li>
         </ul>
       }
     </nav>
