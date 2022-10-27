6dbcdb957f74e198e28c3e683db4f818369bb641
mstevenson <mbstevenson96@gmail.com>
2022-10-27 18:07:54 -0400

    fix errors with pet records deleting

--- src/components/VetRecordCard/VetRecordCard.jsx
+++ src/components/VetRecordCard/VetRecordCard.jsx
@@ -3,16 +3,16 @@ const VetRecordCard = ({ record }) => {
   return (
     <>
       <div>
-        <h3>{record.name}</h3>
-        <h3>{record.contact}</h3>
-        <h3>{record.description}</h3>
-        <h3>{record.diagnosis}</h3>
-        <h3>{record.results}</h3>
-        <h3>{record.medications}</h3>
-        <h3>{record.weight}</h3>
-        <h3>{record.cost}</h3>
-        <h3>{record.appointment}</h3>
-        <h3>{record.rabies}</h3>
+        <h3>Vet Name: {record.name}</h3>
+        <h3>Vet Contact Info: {record.contact}</h3>
+        <h3>Vet Visit Description: {record.description}</h3>
+        <h3>Diagnosis: {record.diagnosis}</h3>
+        <h3>Results{record.results}</h3>
+        <h3>Medications:{record.medications}</h3>
+        <h3>Pet Weight:{record.weight}</h3>
+        <h3>Vet Visit Cost:{record.cost}</h3>
+        <h3>Follow-Up Appointment: {record.appointment}</h3>
+        <h3>Rabies Follow-Up: {record.rabies}</h3>
       </div>
     </>
   )
--- src/pages/PetRecords/PetRecords.jsx
+++ src/pages/PetRecords/PetRecords.jsx
@@ -1,11 +1,13 @@
 import { useParams, Link } from "react-router-dom"
 import { useEffect, useState } from 'react'
 import * as vetService from '../../services/vetService'
-
 import VetRecordCard from "../../components/VetRecordCard/VetRecordCard";
+import styles from './PetRecords.module.css'
 
 const PetRecords = () => {
   const { id } = useParams()
+
+  console.log('this is the PetRecords id', id);
 
   const [records, setRecords] = useState([])
   
@@ -17,23 +19,25 @@ const PetRecords = () => {
     fetchRecords()
   }, [id])
 
-  const handleDeleteVet = async id => {
-    const deletedRecord = await vetService.deleteOne(id)
+  const handleDeleteVet = async recordId => {
+    const deletedRecord = await vetService.deleteOne(recordId)
     setRecords(records.filter(record => record._id !== deletedRecord._id))
   }
   
   return ( 
     records?.length &&
-    <main styles={{marginTop: "200px"}}>
+    <main>
       <article>
         <header>
           <h1>This is the Pet Records Page!</h1>
-          {records?.map(record => 
-            <div key={record._id}>
-              <VetRecordCard record={record}/>
-              <button onClick={() => handleDeleteVet(record._id)}>Delete Vet Record</button>
-            </div>
-          )}
+          <div className={styles.displayCards}>
+            {records?.map(record => 
+              <div key={record._id} className={styles.recordContainer}>
+                <VetRecordCard record={record}/>
+                <button onClick={() => handleDeleteVet(record._id)}>Delete Vet Record</button>
+              </div>
+            )}
+          </div>
         </header>
       </article>
     </main>
